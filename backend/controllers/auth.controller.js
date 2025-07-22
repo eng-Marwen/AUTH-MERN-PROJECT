af5a39fe27947049b0e3859f0512dfeb9aae8c85
Marwen Boussabat <boussabatmarwen9@gmail.com>
2025-07-22 22:37:12 +0100

    auth controller created

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup=async (req,res)=>{
+    res.send("yyy world")
+};
+export const login=async (req,res)=>{
+    res.send("yyy 88world")
+};
+export const logout=async (req,res)=>{
+    res.send("yyy99 world")
+}
