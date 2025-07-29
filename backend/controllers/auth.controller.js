2375425356e6327d1639fdc4a4561a3afaf301c5
Marwen Boussabat <boussabatmarwen9@gmail.com>
2025-07-29 18:17:12 +0100

    modifying signup controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,90 +1,144 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import crypto from 'crypto';
+import crypto from "crypto";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificatinMail,sendWemcomeEmail } from "../mailtrap/emails.js";
-export const signup = async (req, res) => {
+import {
+  sendVerificatinMail,
+  sendWemcomeEmail,
+  sendLinkForResettingPwd,
+} from "../sendingMails/emails.js";
+import { send } from "process";
 
-  let { email,lastname,name, password } = req.body;
+export const signup = async (req, res) => {
   try {
-    if (!email || !name || !password) {
-      throw new Error("ALL THE FIELDS ARE REQUIRED");
+    let { email, lastname, name, password } = req.body;
+    if (!email || !password) {
+      throw new Error("EMAIL AND PASSWORD ARE REQUIRED!");
     }
     const isExisted = await User.findOne({ email });
     if (isExisted) throw new Error("USER ALREADY EXISTS");
-    const verificationToken =Math.floor(100000 + Math.random() * 900000);//6 digits code
+    const verificationToken = Math.floor(100000 + Math.random() * 900000); //6 digits code
 
-    const verificationTokenExpiresAt= new Date(Date.now() + 3600 * 1000*24);
+    const verificationTokenExpiresAt = new Date(Date.now() + 3600 * 1000 * 24); // This sets the expiration time to 24 hour(in ms) from now
 
-        // This sets the expiration time to 24 hour(in ms) from now
-
-    password=await bcrypt.hash(password,10);
-    let user = await User.create({
-        name,
-        lastname,
-        email,
-        password,
-        verificationToken,
-        verificationTokenExpiresAt
+    password = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      lastname,
+      email,
+      password,
+      verificationToken,
+      verificationTokenExpiresAt,
     });
     //jwt
-    generateTokenAndSetCookie(res,user._id);
-    await sendVerificatinMail(user.email,verificationToken)
+    generateTokenAndSetCookie(res, user._id);
+    await sendVerificatinMail(user.email, verificationToken);
     res.status(201).json({
       status: "success",
       message: "user created successfully",
-      data:{
+      data: {
         ...user._doc,
-        password:undefined
-      }
+        password: undefined,
+      },
     });
   } catch (error) {
     res.status(400).json({
       status: "fail",
-      message:error.message
+      message: error.message,
     });
   }
 };
-export const verify=async(req,res)=>{
+export const verifyMail = async (req, res) => {
   //1 2 3 6 8 7 form the frontend
-  try{
-    const {code}=req.body;
-    const user=await User.findOne({
-      verificationToken:code,
-      verificationTokenExpiresAt:{$gt:Date.now()}
+  try {
+    const { code } = req.body;
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
     });
-    if(!user){
+    if (!user) {
       return res.status(400).json({
-        status:"fail",
-        message:"invalid or expired verification code"
-      })
+        status: "fail",
+        message: "invalid or expired verification code",
+      });
     }
-    user.isVerified=true;
-    user.verificationToken=undefined;
-    user.verificationTokenExpiresAt=undefined
+    await sendWemcomeEmail(user.email, user.name);
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
     await user.save();
-    await sendWemcomeEmail(user.email,user.name);
     res.status(200).json({
-      status:'success',
-      message:'email verified successfully',
-      data:{
+      status: "success",
+      message: "email verified successfully",
+      data: {
         ...user._doc,
-        password:undefined
-        
-      }
-    })
-  }catch(error){
-    console.log("error in send verification mail")
-     res.status(404).json({
-      status:"failed",
-      message:error.message
-     });
-  };
-
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("error in send verification mail");
+    res.status(404).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
 export const login = async (req, res) => {
-  res.send("yyy 88world");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error("missing email or password field");
+    }
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("user does not exit please signup");
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) throw new Error("invalid password");
+    generateTokenAndSetCookie(res, user.id);
+    user.lastLogin = Date.now();
+    await user.save();
+    res.status(200).json({
+      status: "success",
+      message: "user logged in successfully",
+      data: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
 };
 export const logout = async (req, res) => {
-  res.send("yyy99 world");
+  res.clearCookie("auth-token");
+  res.status(200).json({
+    status: "success",
+    message: "user logged out successfully",
+  });
+};
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("invalid email");
+    const resetPasswordToken = crypto.randomBytes(32).toString("hex");
+    const resetPasswordTokenExpiresAt = Date.now() + 15 * 60 * 1000; //15min
+
+    user.resetPasswordToken=resetPasswordToken;
+    user.resetPasswordTokenExpiresAt=resetPasswordTokenExpiresAt;
+    await user.save();
+
+    await sendLinkForResettingPwd(resetPasswordToken, user.email);
+    res.status(200).json({
+      status: "success",
+      message: "verification mail sent successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, signup,verify } from "../controllers/auth.controller.js";
+import { login, logout, signup,forgotPassword, verifyMail } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/login", login);
-router.get("/logout", logout);
-router.post("/verify",verify)
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/verify-email",verifyMail);
+router.post("/forgot-password",forgotPassword)
 
 export default router;
