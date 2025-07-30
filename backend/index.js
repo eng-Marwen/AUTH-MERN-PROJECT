72cbf5a42cb0e2761d7b551b50e026b92a2192a7
Marwen Boussabat <boussabatmarwen9@gmail.com>
2025-07-30 22:26:12 +0100

    adding the cookie parser function

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,16 @@ import express from "express";
 import { connectDB } from "./DB/connectDB.js"; //use .js when u import local files
 import dotenv from "dotenv"; //+dotenv.config() for accessing to strings in env file(do it just here)
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
-app.use(express.json());//allows us to parse json object from the res.body
-
+app.use(express.json());//allows us to parse incomming requests:req.body (json)
+app.use(cookieParser());//allows us to parse incomming cookies
 const port = process.env.PORT || 2000;
 
 app.use("/api/auth", authRoutes);
-
 
 app.listen(port, () => {
   connectDB();
